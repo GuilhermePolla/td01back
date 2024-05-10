ad3b041a4ab6520b4c7c6d4458ef38e621758128
YanManenti <y.a.martins@outlook.com>
2024-05-10 13:53:52 -0300

    Added route for input filtering, enabled cross-origin requests,  added regex query to get Actor names.

--- src/main/java/org/gy/back/repositories/ActorRepository.java
+++ src/main/java/org/gy/back/repositories/ActorRepository.java
@@ -4,6 +4,8 @@ import org.gy.back.models.Actor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
+
 public interface ActorRepository extends MongoRepository<Actor, String> {
 
     @Query("{id: ?0}")
@@ -12,4 +14,6 @@ public interface ActorRepository extends MongoRepository<Actor, String> {
     @Query("{name: ?0}")
     Actor getByName(String name);
 
+    @Query("{ 'name' : { $regex: ?0, $options: i } }")
+    List<Actor> getByString(String regex);
 }
